258eebe26b16b551e581e8bc1d7c49d76f9b4307
Jon Proch <jmp117@zips.uakron.edu>
2014-02-13 20:09:25 -0500

    Fixed predefined location support in script creation

--- CTRL/CaPSLOC/Scripts/capsloc/createscript.js
+++ CTRL/CaPSLOC/Scripts/capsloc/createscript.js
@@ -6,10 +6,6 @@ $(document).ready(function () {
         $('.command-parameters').css('display', 'none');
         var selectVal = $('#command-list').val();
         $('#command-parameters-' + selectVal).css('display', 'block');
-
-        if (selectVal == 2) {
-            loadLocations();
-        }
     });
 
     scriptRefreshLocations();
@@ -103,11 +99,11 @@ function scriptRefreshLocations(){
                     .appendTo($locs);
                 });
             } else {
-                alert('An error occurred while finding the ALTs: ' + result.data);
+                alert('An error occurred while loading locations: ' + result.data);
             }
         },
         error: function () {
-            alert('An error occurred while finding the ALTs');
+            alert('An error occurred while loading locations');
         }
     });
 }
@@ -135,28 +131,6 @@ function saveScript() {
         },
         error: function () {
             alert('An error occurred while saving the script');
-        }
-    });
-}
-
-function loadLocations() {
-    $('#goto-location-list').empty();  // Remove existing locations
-
-    $.ajax({
-        url: '/CaPSLOC/Map/AllLocations',
-        type: 'GET',
-        success: function (result) {
-            if (result.success) {
-                $.each(result.data, function (index, element) {
-                    $('<option/>').val(element.Id).text(element.Name).appendTo('#goto-location-list');
-                });
-            }
-            else {
-                alert('Error returned from server');
-            }
-        },
-        error: function () {
-            alert('Failed to load locations');
         }
     });
 }
