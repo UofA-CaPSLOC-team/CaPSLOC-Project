7beee08981378375c6435ccb43c613f3624e64fa
Edwin Sarver <els40@zips.uakron.edu>
2014-04-08 12:32:50 -0400

    Added Curtis's stuff to build.

--- ALT/ControlLogic/MCPM/MCPM.cpp
+++ ALT/ControlLogic/MCPM/MCPM.cpp
@@ -1,73 +1,212 @@
-/*
- * MCPM.cpp
- *
- *  Created on: Feb 6, 2014
- *      Author: dt02
- */
-
 #include "MCPM.h"
 
-#define TESTING
-
-MCPM::MCPM() {
-
+MCPM::MCPM()
+{
+	std::cout << "Initializing MCPM..." << std::endl;
+	vertical = new VerticalAxis(0);
+	horizontal = new HorizontalAxis(1);
+	sensors = new SensorManager();
+	//findLimitSwitch();
+	//while(!(abs(sensors->GetMagYRaw()) < 15))
+	/*{
+		findLimitSwitch();
+		findSouth();
+	}*/
+	_degreesFromSouth = 0;
 }
 
-MCPM::~MCPM() {
-
+MCPM::~MCPM()
+{
+	delete sensors;
+	delete vertical;
+	delete horizontal;
 }
 
-bool MCPM::gotoLocation(double dLongitude, double dLatitude, double dAltitude, std::string strLocName){
-
-
-#ifdef TESTING
-	std::cout << "---> Going to " << strLocName <<":\n\tLongitude: " << dLongitude << "\n\tLatitude: " << dLatitude << "\n\tAltitude: " << dAltitude << std::endl;
-	usleep(500000); //sleep for half a second.
-	return true;
-#endif
+GPSCoordinates MCPM::getGPSCoordinate()
+{
+	float longitutde = sensors->GetLongitude();
+	float latitude = sensors->GetLatitude();
+	return GPSCoordinates(longitutde, latitude);
 }
 
-bool MCPM::capturePicture(CaptureMode tCapMode, long lTimeOnTarget, int nResolution, short sFrameRate = 0){
+bool MCPM::gotoLocation( double dLatitude, double dLongitude, double dAltitude )
+{
+	float tPosNorth = sensors->CalculateHorizontalDegreesOfMovementFromNorth(dLatitude, dLongitude);
+	float verticalOffsetInDegrees = sensors->CalculateVerticalDegreesOfMovement(dLatitude, dLongitude, dAltitude);
+	//TODO: come up with the math to determine the number of degrees and direction we need to turn.
+	//float horDegreesToMove = -tPosNorth + _degreesFromSouth;
+	//moveHorizontalDegrees(horDegreesToMove);
+	moveVerticalDegrees(verticalOffsetInDegrees);
+	return false;
+}
 
-
-#ifdef TESTING
-	if(tCapMode == PIC){
-		std::cout << "---> Capturing PICTURE for " << lTimeOnTarget << " ms at a resolution of "<< nResolution << "p."<< std::endl;
-	} else if(tCapMode == VID){
-		std::cout << "---> Capturing VIDEO for " << lTimeOnTarget << " ms at a resolution of " << nResolution << "p and a framerate of " << sFrameRate << " frames/second." << std::endl;
+bool MCPM::relativeMotion(RelativeDirection tRelDir, double nDegrees)
+{
+	bool rVal = false;
+	if (horIsDoneMoving) 
+	{
+		if (tRelDir == UP)
+		{
+			rVal = moveVerticalDegrees(nDegrees);
+		}
+		else if (tRelDir == DOWN)
+		{
+			rVal = moveVerticalDegrees(-nDegrees);
+		}
 	}
-	usleep(500000); //sleep for half a second.
-	return true;
-#endif
-}
-
-bool MCPM::relativeMotion(RelativeDirection tRelDir, double nDegrees){
-
-
-#ifdef TESTING
-	switch(tRelDir){
-	case RIGHT:
-		std::cout << "---> Moving RIGHT "<< nDegrees << " degrees."<< std::endl;
-		break;
-	case LEFT:
-		std::cout << "---> Moving LEFT "<< nDegrees << " degrees."<< std::endl;
-		break;
-	case UP:
-		std::cout << "---> Moving UP "<< nDegrees << " degrees."<< std::endl;
-		break;
-	case DOWN:
-		std::cout << "---> Moving DOWN "<< nDegrees << " degrees."<< std::endl;
-		break;
-	default:
-		break;
+	if (horIsDoneMoving)
+	{
+		if (tRelDir == LEFT)
+		{
+			rVal = moveHorizontalDegrees(nDegrees);
+		}
+		else if (tRelDir == RIGHT)
+		{
+			rVal = moveHorizontalDegrees(-nDegrees);
+		}
 	}
-	usleep(500000); //sleep for half a second
-	return true;
-#endif
+	return rVal;
 }
 
-GPSCoordinates MCPM::getGPSCoordinate(){
-#ifdef TESTING
-	return GPSCoordinates(43.298, 23.3429);
-#endif
+void MCPM::SetGPSCoordinatesFromGoogle(float latitude, float longitude)
+{
+	if (sensors != NULL)
+	{
+		sensors->setGPSfromGoogle(latitude, longitude);
+	}
+}
+
+void MCPM::SetAltitudeFromGoogle(float altitudeInMeters)
+{
+	if (sensors != NULL)
+	{
+		sensors->setAltitudeFromGoogle(altitudeInMeters);
+	}
+}
+
+bool MCPM::isReadyForNextLocation()
+{
+	bool rVal = false;
+	if(vertIsDoneMoving && horIsDoneMoving)
+	{
+		rVal = true;
+	}
+	return rVal;
+}
+
+bool MCPM::moveVerticalDegrees(int degreesToMove)
+{
+	bool rVal = false;
+	float pitch = sensors->GetPitch();
+	float finalPosition = pitch + degreesToMove;
+	cout << "Pitch: " << pitch << " Final Position: " << finalPosition << endl;
+	if (finalPosition <= 90 && finalPosition >= -90)
+	{
+		rVal = vertical->MoveDegrees(degreesToMove);
+	}
+	return rVal;
+}
+
+bool MCPM::moveHorizontalDegrees(int degreesToMove)
+{
+	bool rVal = false;
+	//cout << "Moving " << degreesToMove << " degrees." << endl;
+	int endingDegrees = degreesMovedFromLimitSwitch + degreesToMove;
+	//cout << "Ending degrees " << endingDegrees << " degrees." << endl;
+
+	if (endingDegrees < 0)
+	{
+		degreesToMove = degreesToMove + 360;
+	}
+	else if (endingDegrees > 360)
+	{
+		degreesToMove = abs(degreesToMove) - 360;
+	}
+	//cout << "Actually going to move " << degreesToMove << endl;
+	rVal = horizontal->MoveDegrees(degreesToMove);
+	if (rVal)
+	{
+		degreesMovedFromLimitSwitch += degreesToMove;
+		_degreesFromSouth += degreesToMove;
+		//cout << "Degrees moved from Limit Switch: " << degreesMovedFromLimitSwitch << endl;
+	}
+	return rVal;
+}
+
+void MCPM::findLimitSwitch()
+{
+	if (!horIsDoneMoving)
+		horizontal->Stop();
+	cout << "Attempting to move -360 degrees" << endl;
+	horizontal->MoveDegrees(-360);
+	while(!horIsDoneMoving)
+	{
+		usleep(50000);
+	}
+}
+
+void MCPM::findSouth()
+{
+	int bounceCount = 0;
+	int magx = 0;
+	int magy = 0;
+	int prevx = 0;
+	int prevy = 0;
+	bool movedLeft = true;
+	bool initialize = true;
+	while(bounceCount < 10)
+	{
+		prevx = magx;
+		prevy = magy;
+		magx = sensors->GetMagXRaw();
+		magy = sensors->GetMagYRaw();
+
+		int difference = abs(abs(prevy)-abs(magy));
+		if (initialize)
+		{
+			initialize = false;
+			relativeMotion(LEFT, 20);
+		}
+		else
+		{
+			if (abs(prevy) > abs(magy)) // keep moving the same direction
+			{
+
+				cout << "Prev Y greater than current y" << endl;
+				if (movedLeft)
+				{
+					cout << "Moving left..." << endl;
+					relativeMotion(LEFT, difference);
+					movedLeft = true;
+				}
+				else
+				{
+					cout << "Moving Right..." << endl;
+					relativeMotion(RIGHT, difference);
+					movedLeft = false;
+				}
+			}
+			else //move opposide direction
+			{
+				cout << "Prev Y less than current y" << endl;
+				if (movedLeft)
+				{
+					cout << "Moving right..." << endl;
+					relativeMotion(RIGHT, difference);
+					movedLeft = false;
+				}
+				else
+				{
+					cout << "Moving left..." << endl;
+					relativeMotion(LEFT, difference);
+					movedLeft = true;
+				}
+				bounceCount ++;
+			}
+		}
+		while(!isReadyForNextLocation())
+		{
+			usleep(50000);
+		}
+	}
 }
