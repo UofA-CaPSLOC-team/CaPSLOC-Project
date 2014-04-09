fb83fbbfb08e3e3f67c69a16f9fcbd80f8c98624
Edwin Sarver <els40@zips.uakron.edu>
2014-04-09 23:26:05 +0000

    Check in so Git will stop being such a git

--- ALT/ControlLogic/HI/CameraControl.cpp
+++ ALT/ControlLogic/HI/CameraControl.cpp
@@ -8,10 +8,10 @@
 #include "CameraControl.h"
 
 CameraControl::CameraControl() {
-	if (!Camera.open()) {
-		std::cerr << "Error opening camera" << std::endl;
-	}
-	sleep(3);
+//	if (!Camera.open()) {
+//		std::cerr << "Error opening camera" << std::endl;
+//	}
+//	sleep(3);
 }
 
 CameraControl::~CameraControl() {
@@ -22,7 +22,7 @@ void CameraControl::takeAPicture(std::string filename){
 //	raspicam::RaspiCam Camera; //Camera object
 
 	//Open camera
-//	std::cout <<"Opening Camera..."<< std::endl;
+	std::cout <<"Opening Camera..."<< std::endl;
 //	if (!Camera.open()) {std::cerr << "Error opening camera" << std::endl;
 //	return;
 //	}
@@ -32,20 +32,20 @@ void CameraControl::takeAPicture(std::string filename){
 //	sleep(3);
 
 	//capture
-	Camera.grab();
+//	Camera.grab();
 
 	//allocate memory
-	unsigned char *data = new unsigned char[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
+//	unsigned char *data = new unsigned char[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
 
 	//extract the image in rgb format
-	Camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB );//get camera image
+//	Camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB );//get camera image
 
 	//save
-	std::ofstream outFile (filename.c_str(), std::ios::binary );
-	outFile<<"P6\n" << Camera.getWidth() << " " << Camera.getHeight() <<" 255\n";
-	outFile.write ((char*)data, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+//	std::ofstream outFile (filename.c_str(), std::ios::binary );
+//	outFile<<"P6\n" << Camera.getWidth() << " " << Camera.getHeight() <<" 255\n";
+//	outFile.write ((char*)data, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
 	std::cout << "Image saved at raspicam_image.ppm" << std::endl;
 
 	//free resrources
-	delete data;
+//	delete data;
 }
